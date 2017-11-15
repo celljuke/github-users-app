1df85b889de30f8c86a29ae6efe939f56a6a242c
celljuke <selcukkiraz@gmail.com>
2017-11-15 03:06:58 +0300

    remove token

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 export default {
   GITHUB_GRAPHQL_API_URL: 'https://api.github.com/graphql',
   GITHUB_REST_API_URL: 'https://api.github.com',
-  GITHUB_TOKEN: 'ffbeec9899dac4d52c1eaee13df9710d7e9aee04'
+  GITHUB_TOKEN: ''
 };
